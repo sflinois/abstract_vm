9e400fe0abd6e94a2ae760efe96c9f379aa46920
Sylvain FLINOIS <sflinois@e1r10p4.42.fr>
2019-02-12 19:10:03 +0100

    facto

--- srcs/OperandFactory.cpp
+++ srcs/OperandFactory.cpp
@@ -6,7 +6,7 @@
 /*   By: sflinois <sflinois@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/08 14:03:19 by sflinois          #+#    #+#             */
-/*   Updated: 2019/02/12 14:31:32 by sflinois         ###   ########.fr       */
+/*   Updated: 2019/02/12 19:09:50 by sflinois         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,32 +35,32 @@ IOperand const *	OperandFactory::createOperand(eOperandType type, std::string co
 }
 
 IOperand const *	OperandFactory::createInt8(std::string const &value) const{
-	int val = std::atoi(value.c_str());
+	int val = std::stoi(value);
 
 	if (val < INT8_MIN || val > INT8_MAX)
 		throw std::out_of_range("EXCEPTION: int8 out of range");
 	return (new Operand<int8_t>(static_cast<int8_t>(val)));
 }
 IOperand const *	OperandFactory::createInt16(std::string const &value) const{
-	int val = std::atoi(value.c_str());
+	int val = std::stoi(value);
 
 	if (val < INT16_MIN || val > INT16_MAX)
 		throw std::out_of_range("EXCEPTION: int8 out of range");
 	return new Operand<int16_t>(static_cast<int16_t>(val));
 }
 IOperand const *	OperandFactory::createInt32(std::string const &value) const{
-	int val = std::atoi(value.c_str());
+	int val = std::stoi(value);
 
 	return new Operand<int32_t>(static_cast<int32_t>(val));
 }
 IOperand const *	OperandFactory::createFloat(std::string const &value) const{
-	double val = std::atof(value.c_str());
+	double val = std::stof(value);
 
 	if (val < FLT_MIN|| val > FLT_MAX)
 		throw std::out_of_range("EXCEPTION: int8 out of range");
 	return new Operand<float>(static_cast<float>(val));
 }
 IOperand const *	OperandFactory::createDouble(std::string const &value) const{
-	double val = std::atof(value.c_str());
+	double val = std::stod(value);
 	return new Operand<double>(val);
 }
