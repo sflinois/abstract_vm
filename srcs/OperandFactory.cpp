6367444ed9c168c6bbdbe61ef6ebe95295007dde
Sylvain FLINOIS <sflinois@e2r8p6.42.fr>
2019-06-01 13:52:48 +0200

    changed exception in OperandFactory.cpp

--- srcs/OperandFactory.cpp
+++ srcs/OperandFactory.cpp
@@ -6,7 +6,7 @@
 /*   By: sflinois <sflinois@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/08 14:03:19 by sflinois          #+#    #+#             */
-/*   Updated: 2019/06/01 13:25:26 by sflinois         ###   ########.fr       */
+/*   Updated: 2019/06/01 13:50:39 by sflinois         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,32 +35,78 @@ IOperand const *	OperandFactory::createOperand(eOperandType type, std::string co
 }
 
 IOperand const *	OperandFactory::createInt8(std::string const &value) const{
-	int val = std::stoi(value);
+	int val = 0;
+	
+	try
+	{
+		val = std::stoi(value);
+	}
+	catch (const std::exception& e)
+	{
+		throw std::out_of_range("runtime_error: int8 out of range");
+	}
 
 	if (val < INT8_MIN || val > INT8_MAX)
 		throw std::out_of_range("runtime_error: int8 out of range");
 	return (new Operand<int8_t>(static_cast<int8_t>(val)));
 }
 IOperand const *	OperandFactory::createInt16(std::string const &value) const{
-	int val = std::stoi(value);
+	int val = 0;
+	
+	try
+	{
+		val = std::stoi(value);
+	}
+	catch (const std::exception& e)
+	{
+		throw std::out_of_range("runtime_error: int16 out of range");
+	}
 
 	if (val < INT16_MIN || val > INT16_MAX)
-		throw std::out_of_range("runtime_error: int8 out of range");
+		throw std::out_of_range("runtime_error: int16 out of range");
 	return new Operand<int16_t>(static_cast<int16_t>(val));
 }
 IOperand const *	OperandFactory::createInt32(std::string const &value) const{
-	int val = std::stoi(value);
+	int val = 0;
+	
+	try
+	{
+		val = std::stoi(value);
+	}
+	catch (const std::exception& e)
+	{
+		throw std::out_of_range("runtime_error: int32 out of range");
+	}
 
 	return new Operand<int32_t>(static_cast<int32_t>(val));
 }
 IOperand const *	OperandFactory::createFloat(std::string const &value) const{
-	double val = std::stof(value);
-
+	double val = 0;
+	
+	try
+	{
+		val = std::stof(value);
+	}
+	catch (const std::exception& e)
+	{
+		throw std::out_of_range("runtime_error: float out of range");
+	}
+	
 	if (val < FLT_MIN|| val > FLT_MAX)
-		throw std::out_of_range("runtime_error: int8 out of range");
+		throw std::out_of_range("runtime_error: float out of range");
 	return new Operand<float>(static_cast<float>(val));
 }
 IOperand const *	OperandFactory::createDouble(std::string const &value) const{
-	double val = std::stod(value);
+	double val = 0;
+	
+	try
+	{
+		val = std::stod(value);
+	}
+	catch (const std::exception& e)
+	{
+		throw std::out_of_range("runtime_error: double out of range");
+	}
+	
 	return new Operand<double>(val);
 }
